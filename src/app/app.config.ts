6fe5a942b0548b4595060ce8c3af2a4c78ae9763
Parth Sinha <parth.sinha@webcurl.co.uk>
2025-11-13 16:09:08 +0000

    added prime ng to the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
