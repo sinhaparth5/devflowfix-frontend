9b412a570328352a2b9664d63fcf6b829832cfd4
p4rth <parth_sinha@icloud.com>
2025-11-15 09:04:28 +0000

    updated app config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,33 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  NbThemeModule,
+  NbToastrModule,
+  NbLayoutModule,
+  NbMenuModule,
+  NbSidebarModule
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
-  ]
+    importProvidersFrom(
+      NbThemeModule.forRoot({ name: 'default' }),
+      NbToastrModule.forRoot(),
+      NbLayoutModule,
+      NbMenuModule.forRoot(),
+      NbSidebarModule.forRoot(),
+      NbEvaIconsModule
+    ),
+  ],
 };
