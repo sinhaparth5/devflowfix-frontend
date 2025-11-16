c6d0ac3bdf482d0c36e8c7521ef7fd18ed3f56c6
Parth <parth_sinha18@proton.me>
2025-11-16 22:38:03 +0000

    added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
 import { Routes } from '@angular/router';
-import { Login } from './auth/login/login';
-import { Register } from './auth/register/register';
+import { AUTH_ROUTES } from './auth/auth-routing-module';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
+        component: Home,
     },
     {
-        path: 'login',
-        component: Login,
+        path: 'auth',
+        children: AUTH_ROUTES,
     },
     {
-        path: 'register',
-        component: Register,
+        path: '**',
+        redirectTo: '',
     },
 ];
--- src/app/auth/auth-routing-module.ts
+++ src/app/auth/auth-routing-module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-const routes: Routes = [];
+export const AUTH_ROUTES: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(AUTH_ROUTES)],
   exports: [RouterModule]
 })
 export class AuthRoutingModule { }
